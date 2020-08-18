8915c46f5eece8c925e440c8643e67aeed87a01c
Dulguun Battulga <dolugen@gmail.com>
2020-08-18 16:46:34 +0200

    add map placeholder

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
 app = Flask(__name__)
+
+MAPBOX_ACCESS_TOKEN = os.environ.get('MAPBOX_ACCESS_TOKEN')
+if not MAPBOX_ACCESS_TOKEN:
+    print('Mapbox access token is missing.')
 
 POLLUTANTS = [
     ('pm25', 'Particulate matter less than 2.5 micrometers in diameter'),
@@ -26,13 +31,14 @@ def report():
         pollutants[pollutant] = bool(request.args.get(pollutant))
     date_from = request.args.get('dateFrom')
     date_to = request.args.get('dateTo')
-    
+
     return render_template('report.html',
                             place_name=place_name,
                             averaging_time=averaging_time,
                             pollutants=pollutants,
                             date_from=date_from,
-                            date_to=date_to)
+                            date_to=date_to,
+                            mapbox_access_token=MAPBOX_ACCESS_TOKEN)
 
 @app.route('/resources')
 def resources():
